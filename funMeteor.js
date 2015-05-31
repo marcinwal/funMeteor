edf4e3fd937e162ee12b325c8ea2f81001acd3e7
marcin walendzik <marcin.walendzik@gmail.com>
2015-05-31 23:25:27 +0100

    adding two buttons and a player

--- funMeteor.js
+++ funMeteor.js
@@ -8,7 +8,7 @@ if(Meteor.isClient){
 
   Template.leaderboard.helpers({
     'player': function(){
-      return PlayersList.find();
+      return PlayersList.find({},{sort: {score: -1,name: 1}});
     },
     'selectedClass': function(){
       var playerId = this._id;
@@ -16,6 +16,10 @@ if(Meteor.isClient){
       if (playerId == selectedPlayer){
         return "selected";
       }
+    },
+    'showSelectedPlayer': function(){
+      var selectedPlayer = Session.get('selectedPlayer');
+      return PlayersList.findOne(selectedPlayer);
     }
   });
   
@@ -26,8 +30,11 @@ if(Meteor.isClient){
     },
     'click .increament': function () {
       var selectedPlayer = Session.get('selectedPlayer');
-      PlayersList.update(selectedPlayer,{score:5});
-      console.log(selectedPlayer);
+      PlayersList.update(selectedPlayer,{$inc: {score:5}});
+    },
+    'click .decrement': function () {
+      var selectedPlayer = Session.get('selectedPlayer');
+      PlayersList.update(selectedPlayer,{$inc: {score:-5}});
     }
   });
 
