57f65b07e29e08625b32cd9f92d4b90602149ac1
marcin walendzik <marcin.walendzik@gmail.com>
2015-05-31 23:09:22 +0100

    adding sessions

--- funMeteor.js
+++ funMeteor.js
@@ -1,16 +1,34 @@
 PlayersList = new Mongo.Collection('players');
 
 if(Meteor.isClient){
+
   console.log("hello Client");
+
+
 
   Template.leaderboard.helpers({
     'player': function(){
       return PlayersList.find();
+    },
+    'selectedClass': function(){
+      var playerId = this._id;
+      var selectedPlayer = Session.get('selectedPlayer');
+      if (playerId == selectedPlayer){
+        return "selected";
+      }
     }
-
   });
   
-
+  Template.leaderboard.events({
+    'click .player': function(){
+      var playerId = this._id;
+      Session.set('selectedPlayer',playerId);
+    },
+    'click .increament': function () {
+      var selectedPlayer = Session.get('selectedPlayer');
+      console.log(selectedPlayer);
+    }
+  });
 
 }
 if(Meteor.isServer){
