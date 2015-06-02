2f2e088d52836ca2ede274c4b61bbf94dfcbaa5e
marcin walendzik <marcin.walendzik@gmail.com>
2015-06-02 19:09:05 +0100

    adding methods after insecure removal

--- funMeteor.js
+++ funMeteor.js
@@ -6,6 +6,7 @@ if(Meteor.isClient){
 
   Meteor.subscribe('thePlayers');
 
+
   Template.leaderboard.helpers({
     'player': function(){
       var currentUserId = Meteor.userId();
@@ -33,15 +34,15 @@ if(Meteor.isClient){
     },
     'click .increament': function () {
       var selectedPlayer = Session.get('selectedPlayer');
-      PlayersList.update(selectedPlayer,{$inc: {score:5}});
+      Meteor.call('modifyPlayerScore',selectedPlayer,5)
     },
     'click .decrement': function () {
       var selectedPlayer = Session.get('selectedPlayer');
-      PlayersList.update(selectedPlayer,{$inc: {score:-5}});
+      Meteor.call('modifyPlayerScore',selectedPlayer,-5)
     },
     'click .remove': function() {
       var selectedPlayer = Session.get('selectedPlayer');
-      PlayersList.remove(selectedPlayer);
+      Meteor.call('removePlayerData',selectedPlayer);
     }
   });
 
@@ -50,15 +51,12 @@ if(Meteor.isClient){
       event.preventDefault();
       var currentUserId = Meteor.userId();
       var playerNameVar = event.target.playerName.value;
-      PlayersList.insert({
-        name: playerNameVar,
-        score: 0,
-        createdBy: currentUserId
-      });
+      Meteor.call('insertPlayerData',playerNameVar);
     }
   });
-
 }
+
+
 if(Meteor.isServer){
   console.log("hello Server")
 
@@ -66,6 +64,27 @@ if(Meteor.isServer){
     var currentUserId = this.userId;
     return PlayersList.find();
   });
+
+  Meteor.methods({
+    'insertPlayerData': function(playerNameVar){
+      var currentUserId = Meteor.userId();
+      PlayersList.insert({
+        name: playerNameVar,
+        score: 0,
+        createdBy: currentUserId
+      });
+    },
+    'removePlayerData': function(selectedPlayer){
+      var currentUserId = Meteor.userId();
+      PlayersList.remove({_id: selectedPlayer,createdBy: currentUserId});
+    },
+    'modifyPlayerScore': function(selectedPlayer,scoreValue){
+      var currentUserId = Meteor.userId();
+      PlayersList.update({_id:selectedPlayer,createdBy:currentUserId},
+                         {$inc:{score: scoreValue}})
+    }
+  });
+
 }
 
 
